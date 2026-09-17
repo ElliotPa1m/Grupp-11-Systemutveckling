2684217960d2567961b89075035a4182a486b3ec
ElliotPa1m <elliot.palm@hotmail.com>
2026-09-17 10:33:57 +0200

    login html and js code done (hopefully) and changed product.ts code (again) to remove the "forced to be logged in to browse website" part of the code

---
+++ Frontend/js/login.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("login-form")
+const errorMessage = document.getElementById("error-message")
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    const email = document.getElementById("login-email").value
+    const password = document.getElementById("login-password").value
+
+    try {
+        const response = await fetch("http://localhost:3000/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email, password})
+        })
+
+        const data = await response.json()
+        if (!data.success) {
+            errorMessage.textContent = data.error
+            return
+        }
+
+        localStorage.setItem("token", data.token)
+        window.location.href = "homepage.html"
+
+    } catch (error) {
+        console.error(error)
+        errorMessage.textContent = "Something went wrong. Please try again."
+    }
+})
